7c8f5ff3c5fde325d6de3d93ab4c2d93e39a6233
paguero4300 <paguero@digito.pe>
2025-11-30 00:00:45 -0500

    fix : cambio visual

--- resources/views/filament/pages/client-devices.blade.php
+++ resources/views/filament/pages/client-devices.blade.php
@@ -1,18 +1,17 @@
 <x-filament-panels::page>
-    <div class="space-y-4">
-        {{-- Botón para volver --}}
-        <div>
-            <x-filament::button
-                tag="a"
-                href="{{ route('filament.admin.pages.clients-list') }}"
-                icon="heroicon-o-arrow-left"
-                color="gray"
-            >
-                Volver a Clientes
-            </x-filament::button>
-        </div>
-
-        {{-- Tabla --}}
-        {{ $this->table }}
+    {{-- Botón para volver --}}
+    <div class="mb-6 pb-4 border-b border-gray-200 dark:border-gray-700">
+        <x-filament::button
+            tag="a"
+            href="{{ route('filament.admin.pages.clients-list') }}"
+            icon="heroicon-o-arrow-left"
+            color="gray"
+            size="sm"
+        >
+            Volver a Clientes
+        </x-filament::button>
     </div>
+
+    {{-- Tabla --}}
+    {{ $this->table }}
 </x-filament-panels::page>
